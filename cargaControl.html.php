fe792db14476d9aa60035997d68dd88dcb538e33
enriquefnt <enriquefinetti@gmail.com>
2021-10-11 07:20:27 -0300

    muestra tabla caso

--- cargaControl.html.php
+++ cargaControl.html.php
@@ -36,100 +36,58 @@
 </fieldset>
 </div>
 
+
+
+
+
+
+
+
 <div>
-    <p>
+    <table>
+      <table>
+    <thead>
+      <tr>
+        <th>Nombre</th>
+        <th>Area Operativa Nombre</th>
+        <th>Edad (AMD)</th>
+        <th>Fecha Control</th>
+        <th>Peso</th>
+        <th>Talla</th>
+        <th>Z Peso/Edad</th>
+        <th>Z Talla/Edad</th>
+        <th>Z IMC/Edad</th>
+      </tr>
+    </thead>
+    
+
 <?php
 $caso = [];
-  foreach ($casos as $caso) {
-    if (isset($_POST['idPersona'])  && $caso['idPersona']==$_POST['idPersona']) {
-     echo $caso['Nombre'] . ' ' . $caso['Apellido'] .' ' .  $caso['Sexo'] .' ' .  $caso['areaoperativa'] .' ' .$caso['años'] .'A ' . $caso['meses'] .'M ' . $caso['dias'] .'D ' .
-  ' ' . $caso['Peso'].' ' . $caso['Talla'].' ' . $caso['ZPE'].' ' . $caso['ZTE'].' ' . $caso['ZIMC'].'<br> ' 
- ;
-    }
-   }
-?>
+ foreach ($casos as $caso) {
+  if (isset($_POST['idPersona'])  && $caso['idPersona']==$_POST['idPersona']) { ?>
+<tbody>
+<tr>
+        <td><?= htmlspecialchars($caso['Nombre'] . ' ' . $caso['Apellido'], ENT_QUOTES, 'UTF-8'); ?></td>
+        <td><?= htmlspecialchars($caso['areaoperativa'], ENT_QUOTES, 'UTF-8'); ?></td>
+        <td><?= htmlspecialchars($caso['años'] .'A ' . $caso['meses'] .'M ' . $caso['dias'] .'D ', ENT_QUOTES, 'UTF-8'); ?></td>
+        <td><?= htmlspecialchars($caso['FechaControl'], ENT_QUOTES, 'UTF-8'); ?></td>
+        <td><?= htmlspecialchars($caso['Peso'], ENT_QUOTES, 'UTF-8'); ?></td>
+        <td><?= htmlspecialchars($caso['Talla'], ENT_QUOTES, 'UTF-8'); ?></td>
+        <td><?= htmlspecialchars($caso['ZPE'], ENT_QUOTES, 'UTF-8'); ?></td>
+        <td><?= htmlspecialchars($caso['ZTE'], ENT_QUOTES, 'UTF-8'); ?></td>
+        <td><?= htmlspecialchars($caso['ZIMC'], ENT_QUOTES, 'UTF-8'); ?></td>
 
-</p>
-</div>
-
-<!--
-
-<div>
-      
-        
-      <table >
-        
-        <tbody>
-  <thead>
-  <tr>
-    <th>Fecha control</th>
-    <th>Edad</th>
-   
-    <th>Peso</th>
-   
-    <th>Talla</th>
-    <th>Z Peso/Edad</th>
-    <th>Z Peso/edad</th>
-    <th>Z Talla/edad</th>
-    <th>Z IMC/edad</th>
-  </tr>
-  </thead>
-  <tbody>
-  <?php  $caso = [];
-   
-    if (isset($_POST['idPersona'])  && $caso['idPersona']==$_POST['idPersona']); ?>
-
-  <?php foreach ($casos as $caso): ?>
-
+      </tr>
   
-  <tr>
-    <td><?= htmlspecialchars($caso['FechaControl'], ENT_QUOTES, 'UTF-8'); ?></td>
-    <td><?= htmlspecialchars($caso['años'] .'A ' . $caso['meses'] .'M ' . $caso['dias'] .'D ' , ENT_QUOTES, 'UTF-8'); ?>
-    <td align="center"><?= htmlspecialchars($caso['Peso'], ENT_QUOTES, 'UTF-8'); ?></td>
-    <td><?= htmlspecialchars($caso['Talla'], ENT_QUOTES, 'UTF-8'); ?></td>
-    <td><?= htmlspecialchars($caso['ZPE'], ENT_QUOTES, 'UTF-8'); ?></td>
-    <td align="center"><?= htmlspecialchars($caso['ZTE'], ENT_QUOTES, 'UTF-8'); ?></td>
-    <td align="center" ><?= htmlspecialchars($caso['ZIMC'], ENT_QUOTES, 'UTF-8'); ?></td>
-    <td align="center"><?= htmlspecialchars($caso['ZIMCEdad'], ENT_QUOTES, 'UTF-8'); ?></td>
-</tr>
-
-  <?php endforeach; ?>
-
+    </tbody>
+ <?php
+    }
+}//added missing brace
+?>
 </table>
+
+
 </div>
-
-  -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- verlista.html.php
+++ verlista.html.php
@@ -6,6 +6,54 @@
 	<title>Lista</title>
 </head>
 <body>
+
+
+
+
+<div>
+	<table>
+		<thead>
+			<tr>
+				<th>Nombre</th>
+				<th>Area Operativa Nombre</th>
+				<th>Edad (AMD)</th>
+				<th>Fecha Control</th>
+				<th>Peso</th>
+				<th>Talla</th>
+				<th>Z Peso/Edad</th>
+				<th>Z Talla/Edad</th>
+				<th>Z IMC/Edad</th>
+			</tr>
+		</thead>
+
+
+<?php
+$caso = [];
+	foreach ($casos as $caso): ?>
+
+		<tbody>
+			<tr>
+				<td><?= htmlspecialchars($caso['Nombre'] . ' ' . $caso['Apellido'], ENT_QUOTES, 'UTF-8'); ?></td>
+				<td><?= htmlspecialchars($caso['areaoperativa'], ENT_QUOTES, 'UTF-8'); ?></td>
+				<td><?= htmlspecialchars($caso['años'] .'A ' . $caso['meses'] .'M ' . $caso['dias'] .'D ', ENT_QUOTES, 'UTF-8'); ?></td>
+				<td><?= htmlspecialchars($caso['FechaControl'], ENT_QUOTES, 'UTF-8'); ?></td>
+				<td><?= htmlspecialchars($caso['Peso'], ENT_QUOTES, 'UTF-8'); ?></td>
+				<td><?= htmlspecialchars($caso['Talla'], ENT_QUOTES, 'UTF-8'); ?></td>
+				<td><?= htmlspecialchars($caso['ZPE'], ENT_QUOTES, 'UTF-8'); ?></td>
+				<td><?= htmlspecialchars($caso['ZTE'], ENT_QUOTES, 'UTF-8'); ?></td>
+				<td><?= htmlspecialchars($caso['ZIMC'], ENT_QUOTES, 'UTF-8'); ?></td>
+
+			</tr>
+	<?php endforeach; ?>
+		</tbody>
+	</table>
+</div>
+
+
+
+
+<!--
+
 	<div>
 		<p>
 <?php
@@ -19,6 +67,8 @@ $caso = [];
 
 </p>
 </div>
+-->
+
 <div>
 <a href="index.html">Volver</a>
 <br><br>
